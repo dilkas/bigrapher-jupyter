5c4ad101ee44c8b05996ac5c71377b373becb752
Paulius Dilkas <paulius.dilkas@gmail.com>
2018-07-11 12:53:42 +0100

    Mouseover bigraph preview now works on diagrams generated after the document was loaded

--- custom/custom.js
+++ custom/custom.js
@@ -1,10 +1,31 @@
-$(document).ready(function(){
-    $('#header').prepend('<div class="popup" style="display:none;"></div>');
-    $('.node').on('mouseenter', function(evt){
+function registerHoverEvent () {
+    $('.node').on('mouseenter', function (evt) {
         $('.popup').html('<img src="' + $('title', this).html() + '.svg">');
-        $('.popup').css({left: evt.pageX+30, top: evt.pageY-15}).show();
-        $(this).on('mouseleave', function(){
+        $('.popup').css({ left: evt.pageX + 30, top: evt.pageY - 15 }).show();
+        $(this).on('mouseleave', function () {
             $('.popup').hide();
         });
     });
+}
+
+$(document).ready(function () {
+    $('#header').prepend('<div class="popup" style="display:none;"></div>');
+    registerHoverEvent();
+
+    var target = document.getElementById('notebook');
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            addedNodes = $(mutation.addedNodes);
+            if (addedNodes != null) {
+                addedNodes.each(registerHoverEvent);
+            }
+        });
+    });
+
+    var config = {
+        childList: true,
+        subtree: true
+    };
+
+    observer.observe(target, config);
 });
